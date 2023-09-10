79cdc302dfbe180dacd1908bc98df87d64fbff62
Olesia Lesechko <lesechklesia@gmail.com>
2023-09-10 13:19:27 +0300

    changed num2

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from func import sum_numbers
 
 if __name__ == '__main__':
     num1 = 5
-    num2 = 10
+    num2 = 15
     total = sum_numbers(num1, num2)
     print(f"Сумма чисел {num1} и {num2} равна {total}")
